d707a39d06ce625b8afa974c10c04bd7d6c12fca
kermit <61139827+kermit-xrp@users.noreply.github.com>
2023-09-09 23:46:50 +0100

    Update FundME.sol

--- contracts/FundME.sol
+++ contracts/FundME.sol
@@ -45,24 +45,40 @@ contract FundME {
     projects[projectId].contributors[msg.sender] = true;
 
     emit ContributionReceived(projectId, msg.sender, amount);
-  }
 
-  function fundProject(uint256 projectId) public {
-    require(projects[projectId].amountRaised >= projects[projectId].fundingGoal, "Project has not met funding goal");
+    if (projects[projectId].amountRaised >= projects[projectId].fundingGoal) {
+      // Fund the project if it has met its funding goal
+      projects[projectId].creator.transfer(address(this).balance);
 
-    projects[projectId].creator.transfer(address(this).balance);
-
-    emit ProjectFunded(projectId);
+      emit ProjectFunded(projectId);
+    }
   }
 
   function failProject(uint256 projectId) public {
     require(projects[projectId].amountRaised < projects[projectId].fundingGoal, "Project has met funding goal");
 
+    // Fail the project if it has not met its funding goal
     for (address contributor in projects[projectId].contributors) {
       contributor.transfer(msg.sender.balance);
     }
 
     emit ProjectFailed(projectId);
   }
-}
 
+  function fundProjectFromOtherChain(uint256 projectId, uint256 amount, address chainAddress) public payable {
+    require(projects[projectId].deadline > block.timestamp, "Project is no longer accepting contributions");
+    require(amount > 0, "Amount must be greater than 0");
+    require(chainAddress != address(0), "Invalid chain address");
+
+    // Use the State Connector protocol to get the current price of the cryptocurrency being used to fund the project
+    uint256 currentPrice = StateConnector.getPrice(chainAddress, msg.value);
+
+    // Use the Flare Time Series Oracle protocol to get the current exchange rate between the cryptocurrency being used to fund the project and the native currency of the Flare Network
+    uint256 exchangeRate = FTSO.getExchangeRate(chainAddress, msg.value);
+
+    // Fund the project by transferring the equivalent amount of the native currency of the Flare Network to the project creator
+    projects[projectId].creator.transfer(amount * exchangeRate * currentPrice);
+
+    emit ProjectFunded(projectId);
+  }
+}
